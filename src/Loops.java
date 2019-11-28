071a9f9273c0fb03631066b2f40e5119acebcd17
Vibhay Dhoke <vdhoke2010@gmail.com>
2019-11-28 07:55:39 +0000

    First commit

---
+++ src/Loops.java
@@ -0,0 +1,7 @@
+public class Loops {
+
+	public static void main(String[] args) {
+
+			
+	}
+}
---
+++ src/TestClass.java
@@ -0,0 +1,8 @@
+public class TestClass {
+	public static void main(String arg[]) {
+		String name = "share/* paul"
+				*/, Harry";
+		System.out.println(name);
+		}
+	}
+
